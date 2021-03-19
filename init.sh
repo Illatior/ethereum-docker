bdd066b8f739f00244531d14085d54664a5839c5
Maksim Levkin <aquamarineasf@gmail.com>
2021-03-19 23:16:44 +0200

    add hardcoded connectionstring to bootnode

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 docker-compose -f docker-compose.yml \
                -f docker-compose.init.yml \
                up -d \
-               node_1 node_2 miner_1 miner_2 miner_3
+               node_1 node_2 miner_1 miner_2 miner_3 bootnode
 
